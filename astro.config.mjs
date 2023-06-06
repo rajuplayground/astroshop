533786905d588417c1534a2613134c02a76e28a0
Raju George <test@example.com>
2023-06-06 10:51:27 +0530

    added news letter too

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
-    server: {
-        port: 4000
-    }
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [tailwind(), react()],
+  server: {
+    port: 4000
+  },
+  adapter: netlify()
 });
